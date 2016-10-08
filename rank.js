94ed5ed923bb79365c5195bc81eb6fd0926a312e
Styx25 <MrStyx25@gmail.com>
2016-10-08 21:40:37 +0200

    Update rank.js

--- rank.js
+++ rank.js
@@ -1,4 +1,6 @@
-API
+API.chatLog("RankPlugin je zapnutý! Verzia 1.0!");
+
+
 API.on(API.CHAT, rankinfo);
 function rankinfo(data){
 var msg = data.message;
@@ -9,5 +11,6 @@ if(msg === "!rankinfo"){
 if(fromid == "4515471"){
 setTimeout(function(){ API.sendChat("[@"+ from +"] "); }, 2000);
 }
+else
 }
 }
