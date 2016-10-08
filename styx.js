a06fb813257fa4deb648355d764da786dbb7d8d5
Styx25 <MrStyx25@gmail.com>
2016-10-08 15:21:42 +0200

    Update styx.js

--- styx.js
+++ styx.js
@@ -1,1 +1,21 @@
+API.chatLog("plugin je zapnutý! Verzia 1.0!");
 
+API.on(API.CHAT, afk);
+function afk(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === "!afk"){
+setTimeout(function(){ API.sendChat(""+ from +" je AFK!"); }, 2000);
+}
+}
+
+API.on(API.CHAT, back);
+function back(data){
+var msg = data.message;
+var from = data.un;
+
+if(msg === "!back"){
+setTimeout(function(){ API.sendChat(""+ from +" je späť!"); }, 2000);  
+}
+}
