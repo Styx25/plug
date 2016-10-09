d0ed104c813c439aee8813dd05de5eb7e7f53f79
Styx25 <MrStyx25@gmail.com>
2016-10-09 16:33:53 +0200

    Update woot.js

--- woot.js
+++ woot.js
@@ -8,10 +8,16 @@ API.chatLog("AutoWoot je zapnutý! Verzia 1.1.0");
 })();
 
 
-
+API.on(API.click, djbutton);
+function djbutton(data){
+var fromid = data.uid
+		
+if(fromid == "4515471"){
 ;(function(){
   API.on(API.click, callback).trigger(API.click);
   function callback(){
 	$('#dj-button').click();
-  }
+}
+}
+}
 })();
